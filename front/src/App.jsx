2e7bb8a99cee215faa2d5a3b53515655dfe842fd
axelhuguet33 <axelh33@hotmail.fr>
2024-11-06 17:32:51 +0100

    fix map

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,9 +4,10 @@ import MapComponent from "./component/mapComponent";
 
 function App() {
   return (
-    <>
+    <div>
+      <h1>Carte dans un conteneur de 500x500px</h1>
       <MapComponent />
-    </>
+    </div>
   );
 }
 
--- front/src/component/mapComponent.jsx
+++ front/src/component/mapComponent.jsx
@@ -1,76 +1,25 @@
-// mapComponent.js
-import React, { useState, useRef } from "react";
+import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useMap } from "react-leaflet";
-import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-function MapComponent() {
-  const [location, setLocation] = useState(null);
-  const [result, setResult] = useState("");
-  const mapRef = useRef();
-
-  const handleClick = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setLocation({ latitude, longitude });
-
-        // Recentrer la carte sur la position de l'utilisateur
-        const map = mapRef.current;
-        map.flyTo([latitude, longitude], 13); // Centrer et zoomer sur la position
-
-        fetch(`http://localhost:5000/api/check-water`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ latitude, longitude }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.found) {
-              setResult("Un cours d'eau a été trouvé à proximité !");
-            } else {
-              setResult("Aucun cours d'eau trouvé à proximité.");
-            }
-          })
-          .catch((error) => {
-            console.error("Erreur lors de la requête:", error);
-            setResult("Erreur lors de la requête.");
-          });
-      });
-    } else {
-      alert("La géolocalisation n'est pas supportée par votre navigateur.");
-    }
-  };
+const MapComponent = () => {
+  const center = [43.6835848, 7.2273575]; // Coordonnées centrales (ex: Londres)
+  const zoom = 13;
 
   return (
-    <div style={{ height: "200px", width: "200px" }}>
-      {/* Affichage de la carte avec une taille de 200px par 200px */}
+    <div style={{ width: "500px", height: "500px", margin: "auto" }}>
       <MapContainer
-        center={[43.6835848, 7.2273575]} // Position par défaut
-        zoom={2}
-        style={{ height: "200px", width: "200px" }}
-        ref={mapRef}
+        center={center}
+        zoom={zoom}
+        style={{ width: "100%", height: "100%", borderRadius: "50px" }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="© OpenStreetMap contributors"
-        />
-        {location && (
-          <Marker position={[location.latitude, location.longitude]}>
-            <Popup>
-              Vous êtes ici : {location.latitude}, {location.longitude}
-            </Popup>
-          </Marker>
-        )}
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={center}>
+          <Popup>Une localisation centrale</Popup>
+        </Marker>
       </MapContainer>
-
-      {/* Bouton pour récupérer la position et effectuer l'appel API */}
-      <button onClick={handleClick}>
-        Vérifier les cours d'eau à proximité
-      </button>
-      <p>{result}</p>
     </div>
   );
-}
+};
 
 export default MapComponent;
