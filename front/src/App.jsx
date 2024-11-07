f5864174b1291689a2f0a74ca639f332aae5bdce
axelhuguet33 <axelh33@hotmail.fr>
2024-11-07 09:24:11 +0100

    add inputAdress

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import MapComponent from "./component/mapComponent";
+import InputAddress from "./component/inputAddress";
 
 function App() {
   return (
     <div>
       <h1>Carte dans un conteneur de 500x500px</h1>
       <MapComponent />
+      <InputAddress />
     </div>
   );
 }
