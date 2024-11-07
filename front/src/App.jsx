3e9b584107f22f8e84485f77b66d06ba7de2276d
steven marco <steven.marco@epitech.eu>
2024-11-07 14:28:10 +0100

    dynamic page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import PageArticles from "./component/PageArticles";
 import ArticleMap from "./component/articleMap";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div style={{paddingTop: "100px"}}>
+      <PageArticles />
       <ArticleMap />
     </div>
 
