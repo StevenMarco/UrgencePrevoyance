9c90104ac788bacd4ac6f45b6b7cb3b67ef3caae
axelhuguet33 <axelh33@hotmail.fr>
2024-11-07 19:13:45 +0100

    add scroll to method

--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -1,14 +1,41 @@
+import React, { useRef } from "react";
 import PageArticles from "./component/PageArticles";
 import ArticleMap from "./component/articleMap";
-import "./Home.css";
 import PagePartenaires from "./component/partenaires";
+import "./Home.css";
 
 function Home() {
+  const articlesRef = useRef(null);
+  const mapRef = useRef(null);
+  const partenairesRef = useRef(null);
+
+  const scrollToRef = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div style={{paddingTop: "100px"}}>
-      <PageArticles />
-      <ArticleMap />
-      <PagePartenaires />
+    <div style={{ paddingTop: "80px" }}>
+      <div style={{ top: "10px", right: "10px", display:"flex", justifyContent:"space-around", alignItems:""}}>
+        <button style={{width:"12em"}} onClick={() => scrollToRef(articlesRef)}>
+          Scroll to Articles
+        </button>
+        <button style={{width:"12em"}} onClick={() => scrollToRef(mapRef)}>Scroll to Map</button>
+        <button style={{width:"12em"}} onClick={() => scrollToRef(partenairesRef)}>
+          Scroll to Partenaires
+        </button>
+      </div>
+      <div ref={articlesRef}>
+        <PageArticles />
+      </div>
+      <div ref={mapRef}>
+        <ArticleMap />
+      </div>
+      <div ref={partenairesRef}>
+        <PagePartenaires />
+      </div>
     </div>
   );
 }
