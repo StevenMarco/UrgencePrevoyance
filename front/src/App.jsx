df6d34ee55964234c9ad146ba012119990a94d15
axelhuguet33 <axelh33@hotmail.fr>
2024-11-07 10:22:47 +0100

    add zipcod to address

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import MapComponent from "./component/mapComponent";
-import InputAddress from "./component/inputAddress";
 
 function App() {
   return (
     <div>
-      <h1>Carte dans un conteneur de 500x500px</h1>
+      <h1>Renseignez votre adresse</h1>
       <MapComponent />
-      <InputAddress />
     </div>
   );
 }
