b33954aa49751019c2e5cd357b94b6cd5d811aaa
dbislimi <dbislimi@student.42nice.fr>
2024-11-07 17:51:45 +0100

    01

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import PageArticles from "./component/PageArticles";
 import ArticleMap from "./component/articleMap";
 import "./App.css";
+import PageAccueil from "./component/PageAccueil";
 
 function App() {
   return (
     <div style={{paddingTop: "100px"}}>
+      <PageAccueil />
       <PageArticles />
       <ArticleMap />
     </div>
